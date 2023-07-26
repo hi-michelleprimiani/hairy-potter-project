256760f10e2b1e904f6cee1962e1c9c91e5dfda4
Michelle Primiani <primianimichelle@gmail.com>
2023-07-26 14:11:04 -0400

    Created 5 pieces of pottery at the wheel. Chapter 1 complete.

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,19 @@
+// define a variable in the module to have the value of the primary key for each piece of pottery.
+// it should have an initial value of 1
+// define and export a function named makePottery
+// the makePottery function must accept the following values as input (it needs parameters)
+// i. Shape of the piece of pottery, 2. weight of the piece, 3. height of the piece
+// the makePottery function must return an object with the following properties i. shape, 2. weight, 3. height, 4.id (increment this value each time the function is invoked)
+
+let potteryKey = 1;
+
+export const makePottery = (shape, height, weight) => {
+  const potteryPiece = {
+    shape: shape,
+    weight: weight,
+    height: height,
+    id: potteryKey,
+  };
+  potteryKey++;
+  return potteryPiece;
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,24 @@
 // Imports go first
-
-
+import { makePottery } from "./PotteryWheel.js";
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("mug", 5, 3);
+console.log(mug);
 
+let plate = makePottery("plate", 8, 1);
+console.log(plate);
+
+let bowl = makePottery("bowl", 6, 2);
+console.log(bowl);
+
+let vase = makePottery("vase", 4, 7);
+console.log(vase);
+
+let teapot = makePottery("teapot", 7, 4);
+console.log(teapot);
 
 // Fire each piece of pottery in the kiln
 
-
 // Determine which ones should be sold, and their price
 
-
 // Invoke the component function that renders the HTML list
-
-
-
